35cf399397f02c609dc64d6e424ff7ea8b204d07
GULLAPALLIJYOTHIPRAKASH <gsraolic@gmail.com>
2026-01-23 21:08:06 +0530

    Collections UI design completed

--- src/Projects/furniture-store-ui-design/Furniture-store.jsx
+++ src/Projects/furniture-store-ui-design/Furniture-store.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
-import bed from "./assets/bed.png"
 import Banner from "./components/Banner";
+import Collections from "./components/Collections";
 
 function Furniture_store(){
 
@@ -10,17 +10,8 @@ function Furniture_store(){
     return (<>
     <Navbar/>
     <Banner/>
-    {/* collections start */}
-    <div className="collections-container">
-        <div className="collections-center max-w-[1550px] mx-auto px-[20px] py-16">
-            <div className="heading">
-                <h1 className="text-black text-xl font-medium tracking-[2px] mb-5">LOVE WHERE YOU LIVE</h1>
-                <button>VIEW ALL COLLECTION</button>
-            </div>
-        </div>
-    </div>
-    {/* collections end */}
-    
+    <Collections/>
+   
 
     </>)
 
